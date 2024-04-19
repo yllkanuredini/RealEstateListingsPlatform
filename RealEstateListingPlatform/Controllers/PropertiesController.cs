3388ab6f048599be5ad8739b2651589a4c10ec68
Jeta Termkolli <jetaa.termkolli@gmail.com>
2024-04-19 11:00:37 +0200

    Added PropertiesController

---
+++ RealEstateListingPlatform/Controllers/PropertiesController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealEstateListingPlatform.Data;
+using RealEstateListingPlatform.Models;
+
+namespace RealEstateListingPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertiesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PropertiesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Property>>> GetProperties()
+        {
+            if (_context.Properties == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Properties.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Property>> GetProperty(int id)
+        {
+            var property = await _context.Properties.FindAsync(id);
+
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            return property;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Property>> CreateProperty(Property property)
+        {
+            _context.Properties.Add(property);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProperty), new { id = property.Id }, property);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProperty(int id, Property property)
+        {
+            if (id != property.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(property).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PropertyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProperty(int id)
+        {
+            var property = await _context.Properties.FindAsync(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            _context.Properties.Remove(property);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PropertyExists(int id)
+        {
+            return _context.Properties.Any(e => e.Id == id);
+        }
+    }
+}
