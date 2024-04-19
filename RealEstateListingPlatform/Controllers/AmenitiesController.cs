300e2aae587d8e4df3c5b3b25bf3ed3e5b1899d5
gentkoshi <155828361+gentkoshi@users.noreply.github.com>
2024-04-19 14:55:25 +0200

    Added CRUD operations for Amenities

---
+++ RealEstateListingPlatform/Controllers/AmenitiesController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealEstateListingPlatform.Data;
+using RealEstateListingPlatform.Models;
+
+namespace RealEstateListingPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AmenitiesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AmenitiesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Amenity>>> GetAmenities()
+        {
+            if (_context.Amenities == null)
+            {
+                return NotFound();
+            }
+            return await _context.Amenities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Amenity>> GetAmenity(int id)
+        {
+            var amenity = await _context.Amenities.FindAsync(id);
+
+            if (amenity == null)
+            {
+                return NotFound();
+            }
+
+            return amenity;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Amenity>> CreateAmenity(Amenity amenity)
+        {
+            _context.Amenities.Add(amenity);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAmenity), new { id = amenity.Id }, amenity);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAmenity(int id, Amenity amenity)
+        {
+            if (id != amenity.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(amenity).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AmenityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAmenity(int id)
+        {
+            var amenity = await _context.Amenities.FindAsync(id);
+            if (amenity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Amenities.Remove(amenity);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AmenityExists(int id)
+        {
+            return _context.Amenities.Any(e => e.Id == id);
+        }
+    }
+}
