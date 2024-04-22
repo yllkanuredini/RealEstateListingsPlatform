586e62cc8285f3e5693f24806083ea3d8634701a
yllkanuredini <58362102+yllkanuredini@users.noreply.github.com>
2024-04-22 15:18:49 +0200

    commited to Models
    Role.cs and User.cs

---
+++ RealEstateListingPlatform/Models/Role.cs
@@ -0,0 +1,17 @@
+﻿namespace RealEstateListingPlatform.Models
+{
+    public enum Role
+    {
+        Admin,
+        Agent,
+        User
+    }
+
+    public static class RoleExtensions
+    {
+        public static string ToRoleString(this Role role)
+        {
+            return role.ToString();
+        }
+    }
+}
---
+++ RealEstateListingPlatform/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateListingPlatform.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? Role { get; set; }
+    }
+}
